f44a314a8c259a832f2b58680695a0b225d454f8
Cristian Varela Casas <cristianchantada@gmail.com>
2023-12-19 19:45:02 +0100

    commit de casa, programa en proceso

--- src/Bingo.java
+++ src/Bingo.java
@@ -5,31 +5,38 @@ import java.awt.event.ActionListener;
 
 public class Bingo extends JFrame implements ActionListener {
 
+    /**
+     * Set the following configuration variables
+     */
+
+    final String ABSOLUTE_PATH_TO_DIR_IMG = "C:\\Users\\Usuario\\Desktop\\bingo\\bingo\\img";
+    final String IMAGE_NAME = "numerosBingo.jpg";
+
+    // Icono
+    final String IMAGE_PATH = ABSOLUTE_PATH_TO_DIR_IMG + "\\" + IMAGE_NAME;
+    ImageIcon icon = new ImageIcon(IMAGE_PATH);
+    JLabel iconLabel = new JLabel(icon);
+
     JPanel jpanel = (JPanel) this.getContentPane();
     JTextField bingoNumbers = new JTextField();
     JLabel numbersLabel = new JLabel();
     JButton button = new JButton("Vai bola");
     JLabel bingoFinishedLabel = new JLabel();
     JLabel[] imagesBallsArray = new JLabel[20];
-
     int extractions = 1;
     int ballImageXPosition = 25;
 
-    // Icono
-    final String ABSOLUTE_PATH_TO_DIR_IMG = "C:\\DAW\\Programación\\Java2Evaluacion\\bingo\\img";
-    final String IMAGE_NAME = "numerosBingo.jpg";
-    final String IMAGE_PATH = ABSOLUTE_PATH_TO_DIR_IMG + "\\" + IMAGE_NAME;
-    ImageIcon icon = new ImageIcon(IMAGE_PATH);
-    JLabel iconLabel = new JLabel(icon);
-
     public Bingo(){
-        setSize(800,600);
+        setSize(900,900);
         setTitle("Bingooooooooo !!!");
         setResizable(true);
+        setLocation(250, 15);
 
         // Disposición nula para control total de panel
         jpanel.setLayout(null);
-        jpanel.setBackground(Color.magenta);
+
+        Color color = Color.decode("#FFC482");
+        jpanel.setBackground(color);
 
         bingoPanelBuilder();
         setVisible(true);
@@ -37,25 +44,34 @@ public class Bingo extends JFrame implements ActionListener {
     }
 
     public void bingoPanelBuilder(){
-       numbersLabel.setText("Bolas extraídas: ");
-       numbersLabel.setBounds(25, 25, 100, 25);
-       jpanel.add(numbersLabel);
 
-       bingoNumbers.setBounds(new Rectangle(200, 25,800, 25));
-       bingoNumbers.setEditable(false);
-       bingoNumbers.setHorizontalAlignment(JTextField.CENTER);
-       jpanel.add(bingoNumbers);
+        Font font = new Font("Arial", Font.PLAIN, 24);
 
-       button.addActionListener(this);
-       button.setBounds(25, 70, 100, 75);
-       jpanel.add(button);
+        jpanel.add(iconLabel);
+        iconLabel.setBounds(50, 25, 762, 610);
 
-       bingoFinishedLabel.setText("Bingo finalizado");
-       bingoFinishedLabel.setBounds(25, 230, 150, 25);
+        numbersLabel.setText("Bolas extraídas: ");
+        numbersLabel.setBounds(100, 650, 200, 30);
+        numbersLabel.setFont(font);
+        jpanel.add(numbersLabel);
 
-       iconLabel.setBounds(25, 250, 760, 760);
-       //jpanel.add(iconLabel);
+        bingoNumbers.setBounds(new Rectangle(100, 690,675, 30));
+        bingoNumbers.setEditable(false);
+        bingoNumbers.setHorizontalAlignment(JTextField.CENTER);
+        bingoNumbers.setFont(font);
+        jpanel.add(bingoNumbers);
 
+        button.addActionListener(this);
+        button.setBounds(375, 740, 150, 100);
+        jpanel.add(button);
+
+        bingoFinishedLabel.setText("Bingo finalizado");
+        Font finishedLabelFont = new Font("Arial", Font.BOLD, 32);
+        bingoFinishedLabel.setFont(finishedLabelFont);
+        bingoFinishedLabel.setVisible(false);
+        bingoFinishedLabel.setForeground(Color.RED);
+        bingoFinishedLabel.setBounds(100, 750, 325, 40);
+        jpanel.add(bingoFinishedLabel);
 
     }
 
@@ -70,7 +86,7 @@ public class Bingo extends JFrame implements ActionListener {
         System.out.println("extractions = " + extractions);
         if(extractions > 19){
             button.setEnabled(false);
-            jpanel.add(bingoFinishedLabel);
+            bingoFinishedLabel.setVisible(true);
         }
 
         boolean repeated;
@@ -95,9 +111,10 @@ public class Bingo extends JFrame implements ActionListener {
         ++extractions;
         bingoNumbers.setText(actuallyBalls + " " + ball);
 
+    }
 
-
-
+    public static void main(String[] args) {
+        new Bingo();
     }
 
 }
